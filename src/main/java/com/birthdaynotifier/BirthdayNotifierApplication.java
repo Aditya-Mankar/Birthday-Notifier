99e40ad9b850401742eb2f9c452a1fea742be64f
Aditya Mankar <51234261+Aditya-Mankar@users.noreply.github.com>
2021-09-12 15:29:57 +0530

    Initial setup

---
+++ src/main/java/com/birthdaynotifier/BirthdayNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.birthdaynotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birthdaynotifier/BirthdayNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birthdaynotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
